f5107215b0e0b84a1b7e08509ba27aa10183cae2
Munikzen <106283871+Munikzen@users.noreply.github.com>
2023-05-21 07:55:42 -0600

    Add files via upload

--- leds.s
+++ leds.s
@@ -30,16 +30,12 @@ addition:
         ldr     r0, [r7, #4]
         mov     r1, #31 @ if counter < 31
         cmp     r0, r1
-        bgt     reset @ counter = 0;
+        ble     .A1
+        bl      reset @ counter = 0;
         str     r0, [r7, #4] 
 
-        ldr     r1, =GPIOA_ODR
+.A1:
         ldr     r0, [r7, #4]
-        str     r0, [r1]
-        mov     r0, #500
-        bl      delay
-        ldr     r0, [r7, #4]
-
         # Epilogue
         adds    r7, r7, #8
         mov     sp, r7
@@ -54,21 +50,17 @@ subtraction:
         str     r0, [r7, #4] @ backs up counter argument
         # Function body
         ldr     r0, [r7, #4] @ r0 <- counter
-        sub     r0, r0, #1
+        subs    r0, r0, #1
         str     r0, [r7, #4] @ counter--;
         ldr     r0, [r7, #4]
         mov     r1, #0 @ if counter < 0
         cmp     r0, r1
-        ble     reset @ counter = 0;
+        bge     .S1
+        bl      reset @ counter = 0;
         str     r0, [r7, #4] 
 
-        ldr     r1, =GPIOA_ODR
+.S1:
         ldr     r0, [r7, #4]
-        str     r0, [r1]
-        mov     r0, #500
-        bl      delay
-        ldr     r0, [r7, #4]
-
         # Epilogue
         adds    r7, r7, #8
         mov     sp, r7
@@ -131,6 +123,93 @@ F3:     ldr     r0, [r7, #8] @ i < ms
         pop     {r7}
         bx      lr
 
+read_input:
+        # Epilogue
+        push    {r7}
+        sub     sp, sp, #4
+        add     r7, sp, #0
+        str     r0, [r7] 
+        # Function body
+        ldr     r0, =GPIOA_IDR
+        ldr     r0, [r0]
+        ldr     r3, [r7]
+        and     r0, r0, r3
+        cmp     r0, r3
+        beq     .L0
+        mov     r0, #0
+.L0:    
+        # Epilogue
+        adds    r7, r7, #4
+        mov     sp, r7
+        pop     {r7}
+        bx      lr
+
+
+is_button_pressed:
+	push 	{r7, lr}
+	sub	sp, sp, #16
+	add	r7, sp, #0
+	str 	r0, [r7, #4]
+	# read button input
+	ldr	r0, [r7, #4]
+	bl	read_button_input
+	ldr 	r3, [r7, #4]
+	cmp	r0, r3
+	beq	.L1
+	mov	r0, #0
+	adds	r7, r7, #16
+	mov	sp, r7
+	pop 	{r7, lr}
+	bx	lr
+.L1:
+	# counter = 0
+	mov	r3, #0
+	str	r3, [r7, #8]
+	# for (int i = 0, i < 10, i++) 
+	mov     r3, #0 @ j = 0;
+        str     r3, [r7, #12]
+        b       .L2
+.L5:     
+	# wait 5 ms
+	mov 	r0, #50
+	bl   	delay
+	# read button input
+	ldr	r0, [r7, #4]
+	bl	read_button_input
+	ldr 	r3, [r7, #4]
+	cmp	r0, r3
+	beq 	.L3
+	mov 	r3, #0
+	str	r3, [r7, #8]
+.L3:		
+	# counter = counter + 1
+	ldr 	r3, [r7, #8]
+	add		r3, #1
+	str 	r3, [r7, #8]
+	ldr 	r3, [r7, #8]
+	cmp	r3, #4
+	blt	.L4
+	ldr	r0, [r7, #4]
+	adds	r7, r7, #16
+	mov	sp, r7
+	pop 	{r7, lr}
+	bx	lr
+.L4:
+	ldr     r3, [r7, #12] @ j++;
+        add     r3, #1
+        str     r3, [r7, #12]
+.L2:     
+	ldr     r3, [r7, #12] @ j < 10;
+        cmp     r3, #10
+        blt     .L5
+
+	# return 0
+	mov 	r0, #0
+	adds	r7, r7, #16
+	mov		sp, r7
+	pop 	{r7, lr}
+	bx	lr
+
 setup:
         # Prologue
         push    {r7, lr}
@@ -162,38 +241,33 @@ setup:
         str     r0, [r7]
 loop:   
         # if(2 buttons are pressed)
-        ldr     r0, =GPIOA_IDR
-        ldr     r1, [r0]
-        and     r1, 0x300
-        cmp     r1, 0x300
-        bne     .L1
-
+        mov     r0, 0x300
+        bl      is_button_pressed
+        cmp     r0, 0x300
+        bne     .L6
         bl      reset
         str     r0, [r7]
-        mov     r0, #500
-        bl      delay
-
         
-.L1:    # else if(addition button is pressed)
-        ldr     r0, =GPIOA_IDR
-        ldr     r0, [r0]
-        and     r0, 0x100
+.L6:    # else if(addition button is pressed)
+        mov     r0, 0x100
+        bl      is_button_pressed
         cmp     r0, 0x100
-        bne     .L2
-
+        bne     .L7
         ldr     r0, [r7]
         bl      addition
         str     r0, [r7]
 
-.L2:    # else if(subtraction button is pressed)
-        ldr     r0, =GPIOA_IDR
-        ldr     r0, [r0]
-        and     r0, 0x200
+.L7:    # else if(subtraction button is pressed)
+        mov     r0, 0x200
+        bl      is_button_pressed
         cmp     r0, 0x200
-        bne     .L3
+        bne     .L8
 
         ldr     r0, [r7]
         bl      subtraction
         str     r0, [r7]
-.L3:
+.L8:
+        ldr     r1, =GPIOA_IDR
+        ldr     r0, [r7]
+        str     r0, [r1]
         b       loop
